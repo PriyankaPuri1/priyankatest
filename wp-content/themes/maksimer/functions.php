b8bbdf428a5f45dbe0f9eff62ac18e51e68b1279
maksimer <priyanka@maksimer.no>
2017-07-06 13:23:47 +0200

    general

--- wp-content/themes/maksimer/functions.php
+++ wp-content/themes/maksimer/functions.php
@@ -12,6 +12,7 @@
 		add_theme_support( 'menus' );
 		add_theme_support( 'woocommerce' );
 		add_theme_support( 'html5', array(
+			'search-form',
 			'comment-form',
 			'comment-list',
 			'gallery',
